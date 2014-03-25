395000aea2cdc8150b0528a6d81bb502ca9c2d01
Kevin Roark <ker2143@columbia.edu>
2014-03-24 23:50:25 -0400

    added spgh-io

---
+++ spgh-io/index.js
@@ -0,0 +1,49 @@
+
+var sio = require('socket.io');
+var debug = require('debug');
+var msgpack = require('msgpack-js');
+
+process.title = 'spgh-io';
+
+var port = process.env.SPYR_PORT || 3001;
+var io = module.exports = sio(port);
+console.log('listening on *:' + port);
+
+// redis socket.io adapter
+var uri = process.env.SPGH_REDIS || 'localhost:6379';
+io.adapter(require('socket.io-redis')(uri));
+
+// redis queries instance
+var redis = require('./redis')();
+
+var uid = process.env.SPYR_SERVER_UID || port;
+debug('server uid %s', uid);
+
+io.total = 0;
+io.on('connection', function(socket){
+  var req = socket.request;
+
+  // keep track of connected clients
+  updateCount(++io.total);
+  socket.on('disconnect', function(){
+    updateCount(--io.total);
+  });
+
+  // publish yells to redis
+  socket.on('madepic', function(bufs){
+    redis.publish('spgh:madepic', msgpack.encode(bufs));
+  });
+
+});
+
+// sends connections count to everyone
+// by aggregating all servers
+function updateCount(total){
+  redis.hset('spgh:connections', uid, total);
+}
+
+// broadcast events
+function broadcast(socket/*, …*/){
+  var args = Array.prototype.slice.call(arguments, 1);
+  socket.broadcast.emit.apply(socket, args);
+}
